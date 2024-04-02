2b273a69d95d826bd666aee01a13bb7ee5aa6dd6
FlashVagON <djvagon@mail.ru>
2024-04-03 00:56:56 +0500

    index and user

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -1,16 +1,27 @@
 package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.security.Principal;
 
 
 @Controller
-//@RequestMapping("/user")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
 
     @GetMapping("/user")
-    public String userPage() {
+    public String userPage(Model model, Principal principal) {
+        model.addAttribute("userInfo", userService.loadUserByUsername(principal.getName()));
         return "user";
     }
 
